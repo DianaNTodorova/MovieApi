f84cc57df488337cdeffdc03381591997bc5a8bf
Diana Todorova <diana.n.todorova@gmail.com>
2025-07-02 10:18:47 +0200

    Add project files.

---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Year { get; set; }
+        public string Genre { get; set; } = string.Empty;
+        public int Duration { get; set; } 
+
+        //Navigaation property for 1:1 relation
+        public MovieDetails MovieDetails { get; set; } = null!;
+
+        //Navigation property for 1:N relation // ICollection... define a 1-to-many relationship 
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        //Navigation property for M:N relation
+        public ICollection<MovieActor> MovieActors { get; set; } = new List<MovieActor>();
+
+    }
+}
---
+++ MovieApi/Models/MovieDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieApi.Models
+{
+    public class MovieDetails
+    {
+        [Key]
+        [ForeignKey("Movie")]
+        public int MovieId { get; set; }
+        public string Synopsis { get; set; } = string.Empty;
+        public string Language { get; set; } = string.Empty;
+        public int Budget { get; set; }
+        //navigation property to ling Movie and Movie details
+        public Movie Movie { get; set; } = null!;
+
+    }
+}
